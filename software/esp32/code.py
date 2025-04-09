b8ec1d3dbc9068262bf43d7495534546aa9b065f
camporini@protonmail.com <manoah.camporini@erg.school>
2025-04-09 12:34:21 +0200

    movement management

--- software/esp32/code.py
+++ software/esp32/code.py
@@ -11,30 +11,43 @@ from scripts.mqtt import new_mqtt_client
 mqtt_client = new_mqtt_client()
 servo_x = Servo(board.IO5)
 servo_y = Servo(board.IO6)
+data = {}
 
 async def thermal_detection():
+    global data
     while True:
         data = {"matrix": get_flat_pixels()}
         mqtt_client.publish("temperatures", json.dumps(data))
         await asyncio.sleep(0.1)
 
-async def servo_control():
+async def servo_x_control():
     while True:
-        angles = {'x':random.randint(10,180),'y':random.randint(10,180)}
+        '''back and forward of the moving range'''
+        for angle in range(10,170) : 
+            await servo_x.move_to_angle(angle)
+            # mqtt_client.publish("angle_x", json.dumps(angles['x']))
+            await asyncio.sleep(0.05)
+        for angle in range(170,10, -1) : 
+            await servo_x.move_to_angle(angle)
+            # mqtt_client.publish("angle_x", json.dumps(angles['x']))
+            await asyncio.sleep(0.05)           
 
-        await servo_x.move_to_angle(angles['x'])
-        await servo_y.move_to_angle(angles['y'])
-
-        mqtt_client.publish("angle_x", json.dumps(angles['x']))
-        mqtt_client.publish("angle_y", json.dumps(angles['y']))
-
+async def servo_y_control():
+    angle = 90
+    while True:
+        '''move toward the heat source'''
+        y_direction = 1 if max(data["matrix"]) > 32 else -1 # as it is the middle of 64 point flattenend
+        angle += y_direction
+        angle = min(max(angle,80),110) # physical limit (80,110)
+        await servo_y.move_to_angle(angle)
+        #mqtt_client.publish("angle_y", json.dumps(angles['y']))
         await asyncio.sleep(0.05)
-
 
 # define the main function to run the event loop
 async def main():
     asyncio.create_task(thermal_detection())
-    asyncio.create_task(servo_control())
+    asyncio.create_task(servo_x_control())
+    asyncio.create_task(servo_y_control())
 
 # Create and run the event loop
 loop = asyncio.get_event_loop()  
--- software/esp32/scripts/servo.py
+++ software/esp32/scripts/servo.py
@@ -16,32 +16,3 @@ class Servo:
             await asyncio.sleep(0.2)
         self.prev_angle = angle
 
-    
-
-        
-"""
-# I split this method in two (see below)
-# To allow better integration with the mqtt workflow
-# But obviously feel free to change this or revert it back to its previous state :))
-# The only thing is that we should avoid printing stuff and instead send the data on the mqtt broker
-
-async def random_movement(aimed_x_angle=None):
-    try:
-        prev_x_angle
-    except NameError: 
-        prev_x_angle = None
-
-    x_angle = random.randint(10,170) if not aimed_x_angle else aimed_x_angle
-    print(f'new movement')
-    if not prev_x_angle :
-        prev_x_angle = 90
-        await asyncio.sleep(1)
-        
-    for angle in range(prev_x_angle, x_angle, 5):  # 0 - 180 degrees, 5 degrees at a time.
-        servo_x.angle = angle
-        print(f'moving to {angle}')
-        await asyncio.sleep(0.05)
-        
-    prev_x_angle = x_angle
-
-"""
